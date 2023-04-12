897b7ba1b9c953643e6ce1c1ce731df2843e58d9
Michael Maseko <mskmic017@sl-dual-265.cs.uct.ac.za>
2023-04-12 21:55:40 +0200

    2nd commitment with Accounts modified to implement Comparable

--- Account.java
+++ Account.java
@@ -1,8 +1,15 @@
-mport java.util.ArrayList;
+import java.util.ArrayList;
 public class Account implements Comparable<Account>{
     protected String Name;
     protected String Description;
     public String toString(){
         return this.Name;
+    }public ArrayList<Post> Posts = new ArrayList<Post>();
+    public Account(String name, String description){
+        this.Description = description;
+        this.Name = name;
+    }
+    public int compareTo(Account Another){
+        return this.Name.compareTo(Another.Name);
     }
 }
