71997a0e6d729b532c3a99e19a8c627a2d0b8a58
Michael Maseko <mskmic017@sl-dual-265.cs.uct.ac.za>
2023-04-12 21:49:30 +0200

    1st commitment with Account class

---
+++ Account.java
@@ -0,0 +1,8 @@
+mport java.util.ArrayList;
+public class Account implements Comparable<Account>{
+    protected String Name;
+    protected String Description;
+    public String toString(){
+        return this.Name;
+    }
+}
